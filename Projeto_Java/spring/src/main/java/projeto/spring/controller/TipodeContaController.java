f32c9191e38e3961a96a640be2334ba1b25cd2e0
Edmilson <edmilson_ribeiro10@hotmail.com>
2021-10-03 22:43:19 -0300

    Projeto_Java_Spging_boot + .Net_MVC
    Projeto

---
+++ Projeto_Java/spring/src/main/java/projeto/spring/controller/TipodeContaController.java
@@ -0,0 +1,27 @@
+package projeto.spring.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+//import projeto.spring.model.TipodeConta;
+import projeto.spring.repository.TipodeRepo;
+
+@RestController
+@RequestMapping("/contas/tipo")
+@CrossOrigin("*")
+public class TipodeContaController {
+
+    @Autowired
+    private TipodeRepo repo;
+
+    // *********** Buscar em contas pelo tipo de conta ***********
+    @GetMapping("/{tipoConta}") 
+    public Object buscaContaTipo(@PathVariable int tipoConta) {
+        return repo.buscaContaTipo(tipoConta);
+    }
+    
+}
